da1e27f5404c2bb0acce744351ff397afa825107
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2025-06-02 20:28:31 -0600

    two column layout

---
+++ src/routes/Ascii.svelte
@@ -0,0 +1,25 @@
+<h1 class="screenreader">irc.milkmedicine.net</h1>
+<p class="screenreader">prescribed for any and all ailments</p>
+<pre aria-hidden="true">> く__,.ヘヽ.　　　　　　　　/　,ー､ 〉
+> 　　　　　＼ ', !-─‐-i　/　/´
+> 　　　 　 ／｀ｰ'　　　 L/／｀ヽ､
+> 　　 　 /　 ／,　 /|　 ,　 ,　　　 ',
+> 　　　ｲ 　/ /-‐/　ｉ　L_ ﾊ ヽ!　 i
+> 　　　 ﾚ ﾍ 7ｲ｀ﾄ　 ﾚ'ｧ-ﾄ､!ハ|　 |
+> 　　　　 !,/7 '0'　　 ´0iソ| 　      |
+> 　　　　 |.从"　　_　　 ,,,, / |./ 　 |
+> 　　　　 ﾚ'| i＞.､,,__　_,.イ / 　.i 　|
+> 　　　　　 ﾚ'| | / k_７_/ﾚ'ヽ,　ﾊ.　|
+> 　　　　　　 | |/i 〈|/　 i　,.ﾍ |　i　|
+> 　　　　　　.|/ /　ｉ： 　 ﾍ!　　＼　|
+> 　　　 　 　 kヽ>､ﾊ 　 _,.ﾍ､ 　 /､!
+> 　　　　　　 !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+> 　　　　　　 ﾚ'ヽL__|___i,___,ンﾚ|ノ
+> 　　　　　 　　　ﾄ-,/　|___./
+> 　　　　　 　　　'ｰ'　　!_,.:
+>
+> <b>irc.milkmedicine.net</b>
+> <span style="margin-top: -0.5rem; background-color: #007bff; color: #fff;"
+		>~ prescribed for any and all ailments ~ </span>
+>
+</pre>
