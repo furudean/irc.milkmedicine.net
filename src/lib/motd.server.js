2cd9fd2f6b21fead648575c8763c5bae7677b975
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2025-08-19 08:18:01 -0700

    pure random motd

--- src/lib/motd.server.js
+++ src/lib/motd.server.js
@@ -5,14 +5,7 @@ const motds = motd_file
 	.map((line) => line.trim())
 	.filter((line) => line.length > 0)
 
-const ms_in_a_day = 1000 * 60 * 60 * 24
-
-/**
- * @param {Date} [date]
- */
-export function get_motd(date) {
-	const now_ms = date ? date.getTime() : Date.now()
-	const days_since_epoch = Math.floor(now_ms / ms_in_a_day)
-	const index = days_since_epoch % motds.length
+export function get_motd() {
+	const index = Math.floor(Math.random() * motds.length)
 	return motds[index]
 }
