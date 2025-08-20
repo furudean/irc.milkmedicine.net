8dff1f79495afc4bf22b837af502fc1ca0bf27c0
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2025-08-19 18:26:35 -0700

    only runes

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,13 @@ import adapter from '@sveltejs/adapter-node'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	compilerOptions: {
+		runes: true
+	},
 	kit: {
 		adapter: adapter(),
 		prerender: {
-			// eslint-disable-next-line no-unused-vars
-			handleHttpError: ({ path, referrer, message }) => {
+			handleHttpError: ({ path, message }) => {
 				if (path === '/gamja') return
 
 				// otherwise fail the build
