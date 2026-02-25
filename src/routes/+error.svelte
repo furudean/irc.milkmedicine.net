c7bba753e8ec8866cf62c72dd05ebefcc174a5f4
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2026-02-25 13:43:16 -0800

    lol

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,10 +2,18 @@
 	import { page } from '$app/state'
 </script>
 
+<svelte:head>
+	<title>that's bad</title>
+</svelte:head>
+
 <div class="wrapper">
 	<main>
-		<h1>{page.error?.message}</h1>
+		<h1>{page.status} {page.error?.message}</h1>
+		<p>
+			aaaugughhghgudhuauhahhhuhuhuahhauaaauauuuuuahuhaaowauauuwuwowowowoaaoooawuwuwauauaauuwuwuwuwuauwausdusdkksfjksfjkkafjdskfjskdfjklslalfk;f;e;'2'3;4'f;mdw,.svfg,wlsf.;l
+		</p>
 		<img src="bully.png" alt="reisen sad" width="952" height="340" />
+		<p>what have you DONE</p>
 	</main>
 </div>
 
